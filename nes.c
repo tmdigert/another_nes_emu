ec7096bd26783b9c8d8dfc7f0a0ac189d248f216
Akourne Bursche <crocodoctopus@gmail.com>
2021-09-24 23:38:16 -0800

    commenting, add LDA instructions, bug fix

--- nes.c
+++ nes.c
@@ -4,7 +4,37 @@ uint16_t make_u16(uint8_t hi, uint8_t lo) {
 	return ((uint16_t)hi << 8) | (uint16_t)lo;
 }
 
-int test_run(struct Nes* const uint8_t* instructions);
+void init_nes(struct Nes* nes) {
+	// https://wiki.nesdev.org/w/index.php/CPU_power_up_state
+	nes->acc = 0;
+	nes->x = 0;
+	nes->y = 0;
+	nes->sp = 0xFD;
+	nes->status = 0x34;		
+}
+
+void free_nes(struct Nes* nes) { }
+
+void step(struct Nes* nes) {
+	// fetch first opcode, result in Nes::micro_instr
+	uint8_t op = fetch_op(nes);
+
+	//
+	uint8_t cycles = 0xFF; //
+	switch (op) {
+		case 0xA9: cycles = (addrm_imm(nes); lda(nes); 2); break;
+		case 0xA5: cycles = (addrm_zp(nes); lda(nes); 3); break;
+		case 0xB5: cycles = (addrm_zpx(nes); lda(nes); 4); break;
+		case 0xAD: cycles = (addrm_abs(nes); lda(nes); 4); break;
+		case 0xBD: cycles = (uint8_t oops = addrm_abx(nes); lda(nes); 4 + oops); break;
+		case 0xB9: cycles = (uint8_t oops = addrm_abx(nes); lda(nes); 4 + oops); break;
+		case 0xA1: cycles = (addrm_inx(nes); lda(nes); 6); break;
+		case 0xB1: cycles = (uint8_t oops = addrm_iny(nes); lda(nes); 5 + oops); break;
+		default:
+		// unhandled opcode error
+		break;
+	}
+}
 
 /*uint8_t cpu_read(struct Nes*, uint16_t addr) {
 	// range 0..0x1FFF -> internal ram
@@ -31,9 +61,10 @@ void cpu_write(struct Nes*, uint16_t addr, uint8_t byte) {
 
 }*/
 
-void fetch_op(struct Nes* nes) {
-	nes->micro_instr = cpu_read(nes, nes->pc);
+uint8_t fetch_op(struct Nes* nes) {
+	uint8_t op = cpu_read(nes, nes->pc);
 	nes->pc += 1;
+	return op;
 }
 
 void addrm_imp(struct Nes* nes) {
@@ -41,75 +72,107 @@ void addrm_imp(struct Nes* nes) {
 }
 
 void addrm_imm(struct Nes* nes) {
+	// store internal address as operand address
 	nes->micro_addr = nes->pc;
 	nes->pc += 1;
 }
 
 void addrm_zp(struct Nes* nes) {
+	// read operand (zp address), store
 	nes->micro_addr = (uint16_t)cpu_read(nes, nes->pc);
 	nes->pc += 1;
 }
 
 void addrm_zpx(struct Nes* nes) {
-	nes->micro_addr = (uint16_t)cpu_read(nes, nes->pc);
+	// read operand (zp address)
+	uint8_t addr = cpu_read(nes, nes->pc);
 	nes->pc += 1;
-	nes->micro_addr += x;
+	// inc zp address (by y)
+	addr += x;
+	// store internal address
+	nes->micro_addr = (uint16_t)addr;
 }
 
 void addrm_zpy(struct Nes* nes) {
-	nes->micro_addr = (uint16_t)cpu_read(nes, nes->pc);
+	// read operand (zp address)
+	uint8_t addr = (uint16_t)cpu_read(nes, nes->pc);
 	nes->pc += 1;
-	nes->micro_addr += y;
+	// inc zp address (by y)
+	addr += y;
+	// store internal address
+	nes->micro_addr = (uint16_t)addr;
 }
 
 void addrm_inx(struct Nes* nes) {
+	// read operand (zp address)
 	uint8_t addr = cpu_read(nes, nes->pc);
 	nes->pc += 1;
-	addr += x; // wrapping
+	// inc zp address (by x)
+	addr += x;
+	// read zp address (absolute address, low and high byte_)
 	uint8_t lo = cpu_read(nes, addr);
 	uint8_t hi = cpu_read(nes, addr + 1);
+	// store internal address
 	nes->micro_addr = make_u16(hi, lo);
 }
 
-void addrm_iny(struct Nes* nes) {
+uint8_t addrm_iny(struct Nes* nes) {
+	// read operand (zp address) 
 	uint8_t addr = cpu_read(nes, nes->pc);
 	nes->pc += 1;
-	uint8_t hi = cpu_read(nes, addr);
-	uint8_t lo = cpu_read(nes, addr + 1);
+	// read zp address (absolute address, low and high byte)
+	uint8_t lo = cpu_read(nes, addr);
+	uint8_t hi = cpu_read(nes, addr + 1);
+	// inc absolute address (by y), record carry
 	uint8_t carry = lo + new->x < lo;
 	lo += new->y;
 	hi += carry;
+	// store internal address
 	nes->micro_addr = make_u16(hi, lo);
+	return carry;
 }
 
 void addrm_abs(struct Nes* nes) {
+	// read operand (absolute address low byte)
 	uint8_t lo = cpu_read(nes, nes->pc);
 	nes->pc += 1;
+	// read operand (absolute address high byte)
 	uint8_t hi = cpu_read(nes, nes->pc);
 	nes->pc += 1;
+	// store internal address
 	nes->micro_addr = make_u16(hi, lo);
 }
 
-void addrm_abx(struct Nes* nes) {
+uint8_t addrm_abx(struct Nes* nes) {
+	// read operand (absolute address low byte)
 	uint8_t lo = cpu_read(nes, nes->pc);
 	nes->pc += 1;
+	// read operand (absolute address high byte)
 	uint8_t hi = cpu_read(nes, nes->pc);
 	nes->pc += 1;
+	// inc absolute address (by x), record carry
 	uint8_t carry = lo + new->x < lo;
 	lo += new->x;
 	hi += carry;
+	// store internal address
 	nes->micro_addr = make_u16(hi, lo);
+	return carry;
 }
 
-void addrm_aby(struct Nes* nes) {
+uint8_t addrm_aby(struct Nes* nes) {
+	// read operand (absolute address low byte)
 	uint8_t lo = cpu_read(nes, nes->pc);
 	nes->pc += 1;
+	// read operand (absolute address high byte)
 	uint8_t hi = cpu_read(nes, nes->pc);
 	nes->pc += 1;
+	// inc absolute address (by y), record carry
 	uint8_t carry = lo + new->y < lo;
 	lo += new->y;
 	hi += carry;
+	// store internal address
 	nes->micro_addr = make_u16(hi, lo);
+	return carry;
 }
 
 void lda(struct Nes* nes) {
--- nes.h
+++ nes.h
@@ -11,12 +11,14 @@ struct Nes {
 	uint8_t sp;
 	uint8_t status;
 	// CPU micro
-	uint8_t micro_instr;
 	uint16_t micro_addr;
 };
 
-int test_run(struct Nes*, const uint8_t*);
+// init/free
+void init_nes(struct Nes*);
+void free_nes(struct Nes*);
 
+// exec
 int step(struct Nes*);
 
 // read/write
@@ -24,23 +26,22 @@ int step(struct Nes*);
 uint8_t cpu_read(struct Nes*, uint16_t);
 void cpu_write(struct Nes*, uint16_t, uint8_t);
 
-// as far as I'm aware, these two are called for each instruction
+// as far as I'm aware, this is called for reach op
 void fetch_op(struct Nes*);
-void fetch_addr(struct Nes*); // maybe not this one?
 
 // addressing modes (https://wiki.nesdev.org/w/index.php/CPU_addressing_modes)
-void addrm_imp(struct Nes*);
-void addrm_imm(struct Nes*); // LDA #$32 (Const operand)
-void addrm_zp(struct Nes*);  // LDA $32
-void addrm_zpx(struct Nes*); // LDA $32, X
-void addrm_zpy(struct Nes*); // LDA $32, Y
-void addrm_abs(struct Nes*); // LDA $3200
-void addrm_abx(struct Nes*); // LDA $3200, X
-void addrm_aby(struct Nes*); // LDA $3200, Y
-void addrm_rel(struct Nes*); // BNE symbol
-void addrm_ind(struct Nes*); // JMP ($3200)
-void addrm_inx(struct Nes*); // LDA ($32, X)
-void addrm_iny(struct Nes*); // LDA ($32), Y
+void 	addrm_imp(struct Nes*); // ex. ROL A
+void 	addrm_imm(struct Nes*); // ex. LDA #$32
+void 	addrm_zp(struct Nes*);  // ex. LDA $32
+void 	addrm_zpx(struct Nes*); // ex. LDA $32, X
+void 	addrm_zpy(struct Nes*); // ex. LDA $32, Y
+void 	addrm_abs(struct Nes*); // ex. LDA $3200
+uint8_t addrm_abx(struct Nes*); // ex. LDA $3200, X
+uint8_t addrm_aby(struct Nes*); // ex. LDA $3200, Y
+void 	addrm_rel(struct Nes*); // ex. BNE symbol
+void 	addrm_ind(struct Nes*); // ex. JMP ($3200)
+void 	addrm_inx(struct Nes*); // ex. LDA ($32, X)
+uint8_t addrm_iny(struct Nes*); // ex. LDA ($32), Y
 
 // instructions (https://wiki.nesdev.org/w/index.php/6502_instructions)
 void lda(struct Nes*);
