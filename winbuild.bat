9fcb913a64147658e2745986409d201e64813092
Akourne Bursche <crocodoctopus@gmail.com>
2021-10-30 18:26:21 -0800

    reorganize

--- winbuild.bat
+++ winbuild.bat
@@ -1,1 +1,1 @@
-gcc main.c nes.c opcode.c bus.c addressing.c cartridge.c mapper0.c -o emu.exe -lSDL2main -lSDL2 -O3
+gcc src/main.c src/nes.c src/opcode.c src/bus.c src/addressing.c src/cartridge.c src/mapper0.c -o emu.exe -lSDL2main -lSDL2 -O3
